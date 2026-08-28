3e79d6ce05fd59ab1313811c10d523a03e29a1b4
Souvik Chatterjee <souvikchatterjee080@gmail.com>
2026-08-28 19:07:58 +0530

    Update frontend for production deployment

--- apps/frontend/src/lib/axios.ts
+++ apps/frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://ec2-3-110-219-142.ap-south-1.compute.amazonaws.com/",
+    baseURL: "https://agentlens-backend-j3qh.onrender.com/",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
